12fac8d503323f228172f1edc0d9ef3f5bb2e368
lichuangCN <lichuangcn@foxmail.com>
2021-05-10 14:11:02 +0800

    feat:使用fastjson

--- common/src/test/java/site/muzhi/MainTest.java
+++ common/src/test/java/site/muzhi/MainTest.java
@@ -1,12 +1,5 @@
 package site.muzhi;
 
-import com.google.gson.Gson;
-import site.muzhi.result.Result;
-import site.muzhi.result.ResultFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author lichuang
  * @date 2021/05/10
@@ -14,15 +7,6 @@ import java.util.List;
  */
 public class MainTest {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        //Result<List<Integer>> r = ResultFactory.ok(list);
 
-        Result<List<Integer>> result = ResultFactory.ok(list);
-
-        Result<Object> ok = ResultFactory.ok();
-        Gson gson = new Gson();
-        System.out.println(gson.toJson(result));
-        System.out.println(gson.toJson(ok));
     }
 }
